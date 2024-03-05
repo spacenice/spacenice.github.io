faab4448f1ae941ef6190e3a3eb9d8b16c97c5b5
multi <155513558+spacenice@users.noreply.github.com>
2024-03-05 15:38:13 +0800

    Update

--- http_to_https.php
+++ http_to_https.php
@@ -1,11 +1,1 @@
-<?php
-$url = "https://".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI'];
-if (isset($_GET['debug'])) {
-	echo $_SERVER['SERVER_PORT'], $url; exit;
-}
-if ($_SERVER['SERVER_PORT'] == 80) {
-	header("Location:{$url}");
-} else {
-	echo "ok";
-}
-?>	
+
